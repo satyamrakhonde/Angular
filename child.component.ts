07fe36b49f7ce69549ea4b2092ce9e25da226b21
satyamrakhonde <76210917+satyamrakhonde@users.noreply.github.com>
2022-05-12 14:38:17 +0530

    Add files via upload
    Child to Parent Communication

--- child.component.ts
+++ child.component.ts
@@ -1,13 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter } from "@angular/core";
+
 
 @Component({
     selector: 'child',
     template: `<br><h2>Child Component</h2>
-                <input type="text" [value]="appTextBoxValue">`, //This step is optional ..Showing the value of parent component in child component i.e [value] =appTextBoxValue
-                
+                <input type="text" [value]="appTextBoxValue">
+                <input type="text" #childTextBox (keyup)="handleTextBoxChange(childTextBox.value)">`, 
+
+    outputs:['childEvent'], //emit this -->pass this to parent component
     inputs: ['appTextBoxValue']
 })
 
 export class ChildComponent {
         public appTextBoxValue:string = '';
+
+        public childEvent:EventEmitter<string> = new EventEmitter();  //creating field which we want to emit
+        handleTextBoxChange(textBoxValue: any){
+            console.log("handleTextBoxChange()", textBoxValue);
+            this.childEvent.emit(textBoxValue);
+        }
 }
